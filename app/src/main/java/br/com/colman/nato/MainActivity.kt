0ef6e178dbc036bbc3eece4a60530bd0055f68b7
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-11-29 16:36:26 -0500

    🧑‍💻 Add .editorconfig

--- app/src/main/java/br/com/colman/nato/MainActivity.kt
+++ app/src/main/java/br/com/colman/nato/MainActivity.kt
@@ -13,31 +13,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import br.com.colman.nato.ui.theme.NATOAlphabetMemorizerTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            NATOAlphabetMemorizerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      NATOAlphabetMemorizerTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    NATOAlphabetMemorizerTheme {
-        Greeting("Android")
-    }
+  NATOAlphabetMemorizerTheme {
+    Greeting("Android")
+  }
 }
