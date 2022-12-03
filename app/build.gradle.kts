7bb26e45e42de201dd3b9f94bc8432c15fc37c53
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-12-03 16:25:24 -0500

    🎨 Indent build.gradle.kts with 2 spaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,64 +1,64 @@
 import org.gradle.api.JavaVersion.VERSION_1_8
 
 plugins {
-    id("com.android.application")
-    kotlin("android")
-    id("io.gitlab.arturbosch.detekt").version("1.22.0")
+  id("com.android.application")
+  kotlin("android")
+  id("io.gitlab.arturbosch.detekt").version("1.22.0")
 }
 
 android {
-    namespace = "br.com.colman.nato"
-    compileSdk = 32
+  namespace = "br.com.colman.nato"
+  compileSdk = 32
 
-    defaultConfig {
-        applicationId = "br.com.colman.nato"
-        minSdk = 21
-        versionCode = 100
-        versionName = "1.0.0"
+  defaultConfig {
+    applicationId = "br.com.colman.nato"
+    minSdk = 21
+    versionCode = 100
+    versionName = "1.0.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    getByName("release") {
+
     }
 
-    buildTypes {
-        getByName("release") {
+    getByName("debug") {
 
-        }
-
-        getByName("debug") {
-
-        }
     }
+  }
 
-    compileOptions {
-        sourceCompatibility = VERSION_1_8
-        targetCompatibility = VERSION_1_8
-    }
+  compileOptions {
+    sourceCompatibility = VERSION_1_8
+    targetCompatibility = VERSION_1_8
+  }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 
-    buildFeatures {
-        compose = true
-    }
+  buildFeatures {
+    compose = true
+  }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.get()
-    }
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.compose.get()
+  }
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("androidx.compose.ui:ui:${libs.versions.compose.get()}")
-    implementation("androidx.compose.ui:ui-tooling-preview:${libs.versions.compose.get()}")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
-    debugImplementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${libs.versions.compose.get()}")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+  implementation("androidx.activity:activity-compose:1.3.1")
+  implementation("androidx.compose.ui:ui:${libs.versions.compose.get()}")
+  implementation("androidx.compose.ui:ui-tooling-preview:${libs.versions.compose.get()}")
+  implementation("androidx.compose.material3:material3:1.0.0-alpha02")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.3")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
+  debugImplementation("androidx.compose.ui:ui-test-manifest:${libs.versions.compose.get()}")
 }
 
 detekt {
